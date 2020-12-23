8562bd2ec22941399d6341eaf59e53359677a992
Tassio Virginio <tassio@tron.localdomain>
2020-12-22 21:02:19 -0300

    add Procfile Heroku

--- src/main/java/com/tassiovirginio/jnoseanalyze/ApplicationSpringBoot.java
+++ src/main/java/com/tassiovirginio/jnoseanalyze/ApplicationSpringBoot.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ApplicationSpringBoot {
 
     public static void main(String[] args) {
